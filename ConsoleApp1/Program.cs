8a7777b947f326c6b321024d0a7769ae87966bf7
Mikołaj Toczko <s28901@pjwstk.edu.pl>
2024-03-08 09:40:54 +0100

    Add getAvg method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+int[] tab = { 1, 2, 3, 4, 5 };
+Console.Write(GetAvg(tab));
 
 return;
 
 //xd
 //xdddd222
+
+static double GetAvg(int[] tab)
+{
+    int sum = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        sum += tab[i];
+    }
+
+    return sum / tab.Length;
+}
